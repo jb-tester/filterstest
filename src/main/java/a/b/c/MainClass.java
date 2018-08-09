5095ef11d96595ec4b77a7dafa09f5493b11a238
irina <irina@me.com>
2018-08-09 18:16:11 +0300

    just test user input

--- src/main/java/a/b/c/MainClass.java
+++ src/main/java/a/b/c/MainClass.java
@@ -12,7 +12,11 @@ import java.util.Scanner;
 public class MainClass {
 
     public static void main(String[] args) {
-       ReadProps readProps = new ReadProps();
+        Scanner input = new Scanner(System.in);
+        System.out.println("type something: ");
+        String in1 = input.next();
+        System.out.println("you typed: "+in1);
+        ReadProps readProps = new ReadProps();
         try {
             readProps.displayProps("messages.properties", "message1", "message2");
         } catch (IOException e) {
